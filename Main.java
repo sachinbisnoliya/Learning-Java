76aaa5c913a9066fcb27cee97f79c1870ad1b45c
sachinbisnoliya <87073971+sachinbisnoliya@users.noreply.github.com>
2021-07-08 21:08:40 +0530

    Add files via upload
    data types & variables

--- Main.java
+++ Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello World");
+
+	int num1=1;
+	int num2=2;
+	int num3=3;
+    int sum=num1+num2+num3;
+	System.out.println(sum);
     }
 }
